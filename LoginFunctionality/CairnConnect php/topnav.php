9c3a1e3a1e52fa073d0434ca42a38ddcfe905096
Shay Walker <32909114+ShayMWalker@users.noreply.github.com>
2019-05-01 17:03:47 -0400

    Add files via upload

---
+++ LoginFunctionality/CairnConnect php/topnav.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<a class="navbar-brand" href="#">CairnConnect</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item">
+      <a class="nav-link" href="file:///C:/Users/walke/Documents/SE%202019%20Project/UserProfile_Page.html">My Profile<span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="https://cairn.edu/eventcalendar/">Events</a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link" href="https://cairn.edu/give/ways/">Donate</a>
+    </li>
+
+    <li class="nav-item">
+      <a class="nav-link" href="file:///C:/Users/walke/Documents/SE%202019%20Project/HelpDesk_page.html">Help Desk</a>
+    </li>
+
+  </ul>
+  <form class="form-inline my-2 my-lg-0">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+</div>
+</nav>
